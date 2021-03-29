48399d5605d746f10c8d64eeaa937c23ac4a4af9
zhasminmaksatbekova <jasminmaksatbeko@gmail.com>
2021-03-29 15:07:18 +0300

    i added a new project

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
